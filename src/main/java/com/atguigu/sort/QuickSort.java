8b3d34d335ee40e22bcc3ef1f2eb0e119c404863
learn_and_think <ilovesp20@outlook.com>
2021-07-01 19:53:12 +0800

    '尽力学习快速排序和直插排序'

--- src/main/java/com/atguigu/sort/QuickSort.java
+++ src/main/java/com/atguigu/sort/QuickSort.java
@@ -4,21 +4,85 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arraySource = SortUtil.getArray(100);
-        //int[] arraySource = new int[]{5,4,1,3,2};
-        System.out.println(Arrays.toString(arraySource));
+        //int[] arraySource = SortUtil.getArray(100);
+        int[] array = new int[]{6,1,2,7,9,3,4,5,8};
+        System.out.println(Arrays.toString(array));
 
-        int[] arrayResult = quickSort(arraySource);
-        System.out.println(Arrays.toString(arrayResult));
+        quickSort(array,0, array.length-1);
+        System.out.println(Arrays.toString(array));
     }
 
     /**
-     * 对简单选择排序的优化,在每一轮比较时,除了获得最小值,还根据比较结果对其他记录做相应的调整
-     * @param array
-     * @return
+     * 对冒泡排序的优化
+     * 1.选择第一个元素当中间元素,比它小的放左边,比它大的放右边
+     * 2.左边的元素,选第一个当中间元素,比它小的放左边,比它大的放右边,右边的元素,选第一个当中间元素,同样操作
+     * 重复上述过程,直到最底层,形成一个递归,最后回收,就是排序好的,具体实现手段有挖坑法和左右指针法
+     * 快速排序（挖坑法递归）
+     * @param arr   待排序数组
+     * @param low   左边界
+     * @param high  右边界
      */
-    public static int[] quickSort(int[] array) {
-        //代码不会写
-        return array;
+    public static void quickSort(int arr[], int low, int high) {
+        if (arr == null || arr.length <= 0) {
+            return;
+        }
+        if (low >= high) {
+            return;
+        }
+
+        int left = low;
+        int right = high;
+        int temp = arr[left]; //挖坑1：保存基准的值
+
+        while (left < right) {
+            while (left < right && arr[right] >= temp) {
+                right--;
+            }
+            arr[left] = arr[right]; //坑2：从后向前找到比基准小的元素，插入到基准位置坑1中
+            while (left < right && arr[left] <= temp) {
+                left ++;
+            }
+            arr[right] = arr[left]; //坑3：从前往后找到比基准大的元素，放到刚才挖的坑2中
+        }
+        arr[left] = temp; //基准值填补到坑3中，准备分治递归快排
+        System.out.println("Sorting: " + Arrays.toString(arr));
+        quickSort(arr, low, left-1);
+        quickSort(arr, left + 1, high);
+    }
+
+    /**
+     * 快速排序（左右指针法）
+     * @param arr 待排序数组
+     * @param low 左边界
+     * @param high 右边界
+     */
+    public static void sort2(int arr[], int low, int high) {
+        if (arr == null || arr.length <= 0) {
+            return;
+        }
+        if (low >= high) {
+            return;
+        }
+
+        int left = low;
+        int right = high;
+
+        int key = arr[left];
+
+        while (left < right) {
+            while (left < right && arr[right] >= key) {
+                right--;
+            }
+            while (left < right && arr[left] <= key) {
+                left++;
+            }
+            if (left < right) {
+                SortUtil.swap(arr, left, right);
+            }
+        }
+        SortUtil.swap(arr, low, left);
+        System.out.println("Sorting: " + Arrays.toString(arr));
+        sort2(arr, low, left - 1);
+        sort2(arr, left + 1, high);
     }
 }
--- src/main/java/com/atguigu/sort/StraightInsertionSort.java
+++ src/main/java/com/atguigu/sort/StraightInsertionSort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class StraightInsertionSort {
     public static void main(String[] args) {
-        //int[] arraySource = SortUtil.getArray(100);
-        int[] arraySource = new int[]{5,4,1,3,2};
+        int[] arraySource = SortUtil.getArray(100);
+        //int[] arraySource = new int[]{5,4,1,3,2};
         System.out.println(Arrays.toString(arraySource));
 
         int[] arrayResult = straightInsertionSort(arraySource);
@@ -13,13 +13,36 @@ public class StraightInsertionSort {
     }
 
     /**
-     * 直插排序
-     *
+     * 直接插入排序的基本思想是：将数组中的所有元素依次跟前面已经排好的元素相比较，如果选择的元素比已排序的元素小，则交换，直到全部元素都比较过为止。
+     * <li>1.从第一个元素开始，该元素可以认为已经被排序
+     * <li>2.取出下一个元素，在已经排序的元素序列中从后向前扫描
+     * <li>3.如果该元素（已排序）大于新元素，将该元素移到下一位置
+     * <li>4.重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
+     * <li>5.将新元素插入到该位置后
+     * <li>6.重复步骤②~⑤<p>
+     * 有两种写法,第一种是移位法,需要额外临时变量保存待插入数据
      * @param array
      * @return
      */
     public static int[] straightInsertionSort(int[] array) {
-        //添加了一个哨兵,看不懂,等下再看
+        if(array == null || array.length == 0) {
+            return array;
+        }
+
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];//将待排序的数字存入临时变量
+            //要让待排序参数循环跟已排序参数比较,如果小于已排序参数,就让已排序参数往后移1个位置
+            //此时需要找到已排序数组的最后一个数的下标
+            int j = i - 1;
+            while (j >= 0 && array[j] > temp) {//此处注意j要>=0,意思是说当待排数挪到最前面了,就说明它最小
+                //如果temp小于已排参数,就让该已排参数往后移1个,放到j+1上,执行j--
+                array[j+1] = array[j];
+                j--;
+            }
+            //直到array[j]不大于temp,说明不用往后移了,此处要想明白,经过上一轮挪动之后,j--,
+            //此时array[j]<=temp,array[j+1]跟array[j+2]是重复的,[j+2]是挪过去的,[j+1]只是一个遗留的坑,应该用来插入待排序的数字
+            array[j+1] = temp;//此j+1非while中的彼j+1,因为中间有个j--
+        }
         return array;
     }
 }
